82c0edb25535739523dab74280dccbf223ab0fa6
pavel <pavelkor>
2023-03-01 13:59:25 +0600

    Beauty landing

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById("mobile01").innerHTML = '<select><option>Доставка</option><option>Оплата</option><option>Отзывы</option><option>Вопрос - ответ</option><option>Контакты</option></select>';
+
+
+document.getElementById("mobile02").innerHTML = '<select><option>Парикмахерская</option><option>Маникюр</option><option>Педикюр</option><option>Массаж</option><option>Мебель</option></select>'
